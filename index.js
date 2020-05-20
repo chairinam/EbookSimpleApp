79b972ab89659af1518bf74f16198e383c81bc45
Chairina Marsya <chairina.m@photoninfotech.net>
2020-05-20 11:27:36 +0700

    fix folder

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+//import App from './src/App';
+//import App from './src/ebook_app/App';
+import App from './src/internalTraining/App';
+
+AppRegistry.registerComponent(appName, () => App);
