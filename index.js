a17d2b928701a708af3a08653df2a1c8fd89f746
Chairina Marsya <chairina.m@photoninfotech.net>
2020-05-26 10:44:28 +0700

    update App.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-//import App from './src/App';
-//import App from './src/ebook_app/App';
-import App from './src/internalTraining/App';
+import App from './App';
 
 AppRegistry.registerComponent(appName, () => App);
